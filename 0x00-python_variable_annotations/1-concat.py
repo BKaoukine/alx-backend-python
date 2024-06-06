905f6448f5bb4b077ecb89769d5345481822d3e3
bkaoukine <b.koukin@gmail.com>
2024-06-06 21:33:33 +0100

    1. Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Testing typ annonatatio."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concat a func that concat two strings.
+
+    Args:
+        str1 (string): string
+        str2 (string): string
+
+    Returns:
+        string: concat of str1 and str2
+    """
+    return str1+str2
