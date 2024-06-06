1c586b97d393fb288ee00936c0261dcb0636922f
bkaoukine <b.koukin@gmail.com>
2024-06-06 20:59:23 +0100

    0. Basic annotations - add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
+"""Testing typ annonatatio."""
 
 def add(a: float, b: float) -> float:
+    """Add a func that adds two floats.
+
+    Args:
+        a (float): float number
+        b (float): float number
+
+    Returns:
+        float: sum of a and b
+    """
     return a+b
