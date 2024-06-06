b7ffa59c396fe6cc068d8aa8b58519533d7710eb
bkaoukine <b.koukin@gmail.com>
2024-06-06 22:02:34 +0100

    3. Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Testing typ annonatatio."""
+
+
+
+
+
+def to_str(n: float) -> str:
+    """To_str takes float and returns string of it.
+
+    Args:
+        n (float): the float number to be converted
+
+    Returns:
+        string: float as string
+    """
+    return str(n)
