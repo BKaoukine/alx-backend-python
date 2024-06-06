076a5e729355cd31d5cbc34a2cde1b2d257187c6
bkaoukine <b.koukin@gmail.com>
2024-06-06 21:52:01 +0100

    2. Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Testing typ annonatatio."""
+
+
+def floor(n: float) -> int:
+    """Floor takes float and returns it floor.
+
+    Args:
+        n (float): the float number to be rounded
+
+    Returns:
+        int: the rounded number
+    """
+    return floor(n)
