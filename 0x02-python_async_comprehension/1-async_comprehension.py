2f71769956bea4b5510a8c294383ed180270d41b
bkaoukine <b.koukin@gmail.com>
2024-06-11 19:26:27 +0100

    1. Async Comprehensions

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Async Generator.
+
+Yields:
+    Iterator: Random float from 0 to 10
+"""
+import asyncio
+import random
+from typing import List, AsyncGenerator
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> AsyncGenerator[float, None]:
+    """Async_comprehension Coroutine function will loop 10 times.
+
+    each time asynchronously. wait 1 second.
+    then yield a random number between 0 and 10.
+
+    Returns:
+        AsyncGenerator[float,None]: Iterator that holds random floats
+
+    Yields:
+        Iterator[AsyncGenerator[float,None]]: random floats between 0-10
+    """
+    lista = async_generator()
+
+    return [item async for item in lista]
